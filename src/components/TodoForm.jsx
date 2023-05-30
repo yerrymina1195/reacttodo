5bed8f8d0ca233c86c66fac93039c00ef0ac3d01
Makhan Diakho <makhandiakho95@gmail.com>
2023-05-30 20:42:47 +0000

    30/05/ commit

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -4,7 +4,7 @@
 
 export const TodoForm = ({something, onChange,value}) =>{
     return(
-        <form onSubmit={something}   className=" new-item-form d-flex justify-content-center g-1">
+        <form onSubmit={something}   className=" new-item-form d-flex justify-content-center mb-5 g-1">
         <input
         value={value}
           onChange={onChange}
