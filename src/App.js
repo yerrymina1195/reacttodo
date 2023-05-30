e095af2bd0de8a2616950ac5b6469f873e584f0d
Makhan Diakho <makhandiakho95@gmail.com>
2023-05-30 15:50:07 +0000

    30/05

---
+++ src/App.js
@@ -0,0 +1,19 @@
+
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Display } from './components/todo';
+
+function App() {
+
+
+
+  return (
+    <div className="App">
+      <h1 className='bg-primary text-white'>TODO AVEC REACT</h1>
+       <Display/>
+    </div>
+
+  );
+}
+
+export default App;
