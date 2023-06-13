265450b82f8468d2016c1a524d0ae4500d0f35b0
Makhan Diakho <makhandiakho95@gmail.com>
2023-06-13 09:00:36 +0000

    derr

--- src/components/todo.jsx
+++ src/components/todo.jsx
@@ -53,13 +53,34 @@ setTodoTask(todoEditing);
 const editTask = (task, id) => {
   
   const todoEditingFinal=    todoTask.map((todo) =>
-  todo.id === id ? { ...todo, task, isEditing: !todo.isEditing } : todo
+  todo.id === id ? { ...todo, taskName: task, isEditing: !todo.isEditing } : todo
 )
   setTodoTask(todoEditingFinal);
 };
 
 return(
   <>
+    {/* <InputGroup className="mt-3 w-50 mx-auto"   onChange={changemet}>
+    <Form.Control
+      placeholder="New Task"
+      aria-label="Recipient's username"
+      aria-describedby="basic-addon2"
+    />
+    <Button variant="outline-secondary" className='btn btn-success text-white' onClick={ajout} id="button-addon2">
+      ajouter
+    </Button>
+  </InputGroup> */}
+   
+
+      {/* <form onSubmit={ajout}   className=" new-item-form d-flex justify-content-center g-1">
+        <input
+        value={newTask}
+          onChange={changemet}
+          type="text"
+          id="item"
+        />
+      <button  className=" gtr">Add</button>
+    </form> */}
     <TodoForm something={ajout} onChange={changemet} value={newTask} />
    
      {todoTask.map(({taskName ,isEditing ,id,completed})=>
@@ -69,7 +90,10 @@ return(
           <Conteiner taskDisplay={taskName} deleteTask={deleteTask} completedTask={completedTask} completed={completed} editTodo={editTodo} id={id}/>
         )
       )}
-
+       {/* {todoTask.map(({taskName ,isEditing ,id,completed})=>{
+      
+     return  <Conteiner taskDisplay={taskName} deleteTask={deleteTask} completedTask={completedTask} completed={completed} id={id}/>
+    })} */}
   </>
 
 )
